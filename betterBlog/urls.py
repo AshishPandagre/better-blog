874ffe20718529e8604c93666833dc3333e8328f
AshishPandagre <ashishpandagre9@gmail.com>
2021-07-05 00:03:42 +0530

    added an import

--- betterBlog/urls.py
+++ betterBlog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts import git_update_view
 
 urlpatterns = [
 	path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
+    path('update_server/', git_update_view.update, name='update'),
 ]
