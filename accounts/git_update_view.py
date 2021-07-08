1ea1ba36135a9b839da3b298cb7abff5edc224fa
AshishPandagre <ashishpandagre9@gmail.com>
2021-07-08 23:25:20 +0530

    added reloading script

--- accounts/git_update_view.py
+++ accounts/git_update_view.py
@@ -5,6 +5,39 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import os
+import mechanize
+import getpass
+import time
+import json
+
+
+def reload():
+
+    with open(os.path.join(BASE_DIR, 'secrets.json')) as secret_file:
+        secret = json.load(secret_file)
+
+    username = secret['username']
+    password = secret['password']
+    domain = 'betterblog.pythonanywhere.com'
+
+    br = mechanize.Browser()
+    br.set_handle_robots(False)
+    br.open('https://www.pythonanywhere.com/login/')
+    time.sleep(1)
+
+    br.select_form(nr=0)
+    br['username'] = username
+    br['password'] = password
+
+    br.submit()
+    time.sleep(1)
+    resp = br.open('https://www.pythonanywhere.com/user/{username}/webapps/{domain}/reload')
+    print(resp.read())
+    time.sleep(1)
+
+    br.open('https://www.pythonanywhere.com/logout')
+
+
 
 @csrf_exempt
 def update(request):
@@ -18,8 +51,7 @@ def update(request):
         origin = repo.remotes.origin
 
         origin.pull()
-
-        os.system('touch /var/www/betterblog_pythonanywhere_com_wsgi.py')
+        reload()
 
         return HttpResponse("Updated code on PythonAnywhere")
     else:
