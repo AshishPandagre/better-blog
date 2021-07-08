7ab64b2dd1ca3a12451eb48f8a49dca789b75456
AshishPandagre <ashishpandagre9@gmail.com>
2021-07-08 23:45:56 +0530

    removed reload

--- accounts/git_update_view.py
+++ accounts/git_update_view.py
@@ -4,40 +4,7 @@ import git
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
-import os
-import mechanize
-import getpass
-import time
-import json
 from django.conf import settings
-
-
-def reload():
-
-    with open(os.path.join(settings.BASE_DIR, 'secrets.json')) as secret_file:
-        secret = json.load(secret_file)
-
-    username = secret['username']
-    password = secret['password']
-    domain = 'betterblog.pythonanywhere.com'
-
-    br = mechanize.Browser()
-    br.set_handle_robots(False)
-    br.open('https://www.pythonanywhere.com/login/')
-    time.sleep(1)
-
-    br.select_form(nr=0)
-    br['username'] = username
-    br['password'] = password
-
-    br.submit()
-    time.sleep(1)
-    resp = br.open(f'https://www.pythonanywhere.com/user/{username}/webapps/{domain}/reload')
-    print(resp.read())
-    time.sleep(1)
-
-    br.open('https://www.pythonanywhere.com/logout')
-
 
 
 @csrf_exempt
@@ -52,7 +19,6 @@ def update(request):
         origin = repo.remotes.origin
 
         origin.pull()
-        reload()
 
         return HttpResponse("Updated code on PythonAnywhere")
     else:
