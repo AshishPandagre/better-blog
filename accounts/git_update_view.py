1c5149b6574590c060bbbf97f843a08edde26f6a
AshishPandagre <ashishpandagre9@gmail.com>
2021-08-15 10:41:33 +0530

    added auto reload in pythonanywhere

--- accounts/git_update_view.py
+++ accounts/git_update_view.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
+import json
 
 
 @csrf_exempt
@@ -20,6 +21,26 @@ def update(request):
 
         origin.pull()
 
-        return HttpResponse("Updated code on PythonAnywhere")
+        import requests
+    
+        with open(os.path.join(BASE_DIR, 'secrets.json')) as secret_file:
+            secret = json.load(secret_file)
+
+        username = "betterblog"
+        api_token = secret["PYTHONANYWHERE_API_TOKEN"]
+        domain_name = "betterblog.pythonanywhere.com"
+
+        response = requests.post(
+            'https://www.pythonanywhere.com/api/v0/user/{username}/webapps/{domain_name}/reload/'.format(
+                username=username, domain_name=domain_name
+            ),
+            headers={'Authorization': 'Token {token}'.format(token=api_token)}
+        )
+        
+        if response.status_code == 200:
+            return HttpResponse("Updated code on PythonAnywhere")
+        else:
+            return HttpResponse("some error occured.")
+
     else:
         return HttpResponse("Couldn't update the code on PythonAnywhere...")
